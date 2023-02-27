a93bdecddab12634ee205428fc37864d64df16f8
shibi <18861813833@163.com>
2023-02-27 18:42:34 +0800

    Added Lean touch & Build first scene

---
+++ Assets/Scripts/MOFAPlancementAndAbsorbSceneManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class MOFAPlancementAndAbsorbSceneManager : MonoBehaviour
+{
+    [SerializeField]
+    ARRaycastManager m_RaycastManager;
+
+    [SerializeField]
+    GameObject m_CollctableObjcetPrefab;
+
+    GameObject m_CollctableObjectInsatance;
+
+    List<ARRaycastHit> m_Hits = new List<ARRaycastHit>();
+
+    void Update()
+    {
+        if (Input.touchCount == 0)
+            return;
+
+        if (m_RaycastManager.Raycast(Input.GetTouch(0).position, m_Hits))
+        {
+            // Only returns true if there is at least one hit
+            if(m_CollctableObjectInsatance == null)
+            {
+                m_CollctableObjectInsatance = Instantiate(m_CollctableObjcetPrefab);
+            }
+            m_CollctableObjectInsatance.transform.position = m_Hits[0].pose.position + (Vector3.up * 0.75f);
+        }
+    }
+}
---
+++ Packages/com.holoi.holokit/CHANGELOG.md
@@ -0,0 +1,18 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+## [0.0.2] - 2023-02-10
+
+### Changed
+
+- Updated "HoloKit UI Canvas" prefab with a more flexible button to switch render mode.
+
+## [0.0.1] - 2023-01-31
+
+This is the first release of HoloKit Unity SDK Package.
