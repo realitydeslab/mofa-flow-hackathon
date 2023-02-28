0ef867f40eeb5d8015e34fc82500467f9da3a6ad
shibi <18861813833@163.com>
2023-02-28 18:32:50 +0800

    Fixed Icon Shake Bug

--- Assets/Scripts/ObjectShake.cs
+++ Assets/Scripts/ObjectShake.cs
@@ -60,8 +60,8 @@ public class ObjectShake : MonoBehaviour
     {
 		Shake();
 		yield return new WaitForSeconds(1f);
-		Shake();
-		yield return new WaitForSeconds(1f);
-		Shake();
-	}
+        //Shake();
+        //yield return new WaitForSeconds(1f);
+        //Shake();
+    }
 }
